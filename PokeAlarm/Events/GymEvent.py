e8aa329032cc761447184f33b848e24bab4b2131
Wei Zhang <wei.zhang@shopee.com>
2023-01-31 04:05:35 +0800

    Use "ex_raid_eligible" for RDM

--- PokeAlarm/Events/GymEvent.py
+++ PokeAlarm/Events/GymEvent.py
@@ -43,7 +43,7 @@ class GymEvent(BaseEvent):
         ).strip()
         self.gym_image = check_for_none(str, data.get("url"), Unknown.REGULAR)
         self.ex_eligible = check_for_none(
-            int, data.get("is_ex_raid_eligible"), Unknown.REGULAR
+            int, data.get("is_ex_raid_eligible") or data.get("ex_raid_eligible"), Unknown.REGULAR
         )
 
         # Gym Guards
